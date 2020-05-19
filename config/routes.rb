d0f1ebc030d4a59a2557bb3213502bd1e86c0d52
Renato Silva <renato.oliveira.silva@gmail.com>
2020-05-18 23:52:04 -0300

    Scaffolding Model Contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "static_pages#index"
 
-  get 'contact', to: "static_pages#contact"
+  get 'contact_form', to: "static_pages#contact_form"
   get 'about', to: "static_pages#about"
  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_19_023128) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
