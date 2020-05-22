8223a97264bc27aa81d9369ca103f2436e74b6c5
Renato Silva <renato.oliveira.silva@gmail.com>
2020-05-21 22:23:52 -0300

    Create Users Model + i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :contacts
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "static_pages#index"
